961d4f57c7b7f3fdeeab39c017fc39503edd5ba6
OwlMech <pisciapantaloni@proton.me>
2026-09-01 18:50:07 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+/* Offline cache. Bump CACHE when you edit index.html so her phone
+   picks up the new version instead of the old cached one. */
+const CACHE = "ao-v1";
+const FILES = ["./", "./index.html", "./manifest.json", "./icon.png", "./icon-192.png"];
+
+self.addEventListener("install", e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));
+});
+
+self.addEventListener("activate", e=>{
+  e.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", e=>{
+  if(e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(hit=>{
+      if(hit) return hit;
+      return fetch(e.request).then(res=>{
+        if(res.ok && new URL(e.request.url).origin === location.origin){
+          const copy = res.clone();
+          caches.open(CACHE).then(c=>c.put(e.request, copy));
+        }
+        return res;
+      }).catch(()=>caches.match("./index.html"));
+    })
+  );
+});
